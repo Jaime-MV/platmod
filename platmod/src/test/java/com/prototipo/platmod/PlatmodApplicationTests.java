021b6a1833baee1c94831667e63d5328ab58eb37
Jaime-MV <jaime.a.matamoros@gmail.com>
2026-02-07 21:53:27 -0600

    Primer avance

---
+++ platmod/src/test/java/com/prototipo/platmod/PlatmodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prototipo.platmod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatmodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
